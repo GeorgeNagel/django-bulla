4c2e738c0a853b35e20c393645f54b4069648087
George Nagel <gsnagel@gmail.com>
2024-12-18 08:18:39 -0500

    Fix app config name

--- django_bulla/apps.py
+++ django_bulla/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class MusaConfig(AppConfig):
+class BullaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_bulla"
     label = "bulla"
